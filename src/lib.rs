ff92943794fa7fe7ad99b0b93f0bab3658a57617
Matt-Koko <37648823+Matt-Koko@users.noreply.github.com>
2024-03-18 01:48:59 +1100

    draw a square

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,46 @@ use winit::{
     window::Window,
 };
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
+}
+
+// lib.rs
+const VERTICES: &[Vertex] = &[
+    // Triangle 1
+    Vertex { position: [-0.8, -0.8, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [0.8, -0.8, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [0.8, 0.8, 0.0], color: [1.0, 0.0, 0.0] },
+    // Triangle 2
+    Vertex { position: [-0.8, -0.8, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [0.8, 0.8, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [-0.8, 0.8, 1.0], color: [0.0, 0.0, 1.0] },
+];
+
 struct State {
     surface: wgpu::Surface<'static>,
     device: wgpu::Device,
@@ -15,10 +55,16 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
 }
 
 impl State {
     async fn new(window: Arc<Window>) -> Self {
+        use wgpu::util::DeviceExt;
+
+        let num_vertices = VERTICES.len() as u32;
+
         let mut size = window.inner_size();
         size.width = size.width.max(1);
         size.height = size.height.max(1);
@@ -66,13 +112,24 @@ impl State {
         let swapchain_capabilities = surface.get_capabilities(&adapter);
         let swapchain_format = swapchain_capabilities.formats[0];
 
+        // Create the vertex buffer
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: None,
             layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[],
+                buffers: &[
+                    Vertex::desc(),
+                ],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -98,6 +155,8 @@ impl State {
             config,
             size,
             render_pipeline,
+            vertex_buffer,
+            num_vertices,
         }
     }
 
@@ -157,8 +216,8 @@ impl State {
                 });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
-
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..self.num_vertices, 0..1);
         }
 
         
